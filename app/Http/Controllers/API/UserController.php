a1a3b3db468ed632f3a55ca14b413fe126b38b46
Ratino <ratinoray@gmail.com>
2026-07-29 18:04:15 +0700

    user controller api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Exception;
 use App\Actions\Fortify\PasswordValidationRules;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 
 class UserController extends Controller
@@ -90,4 +92,54 @@ class UserController extends Controller
             ], 'Authentication Failed', 500);
         }
     }
+
+    public function logout(Request $request)
+    {
+        $token = $request->user()->currentAccessToken()->delete();
+
+        return ResponseFormatter::success($token, 'Token Revoked');
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success($request->user(), 'Data profile user berhasil diambil');
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'file' => 'required|image|max:2048',
+        ]);
+
+        if($validator->fails())
+            {            
+                return ResponseFormatter::error([
+                    'error' => $validator->errors(),
+                ], 'Update photo fails', 401);
+            }
+        
+            if($request->file('file'))
+            {
+                $file = $request->file->store('assets/user', 'public');
+
+                // Save photo to db
+                $user = Auth::user();
+                $user->profile_photo_path = $file;
+                $user->update();
+
+                return ResponseFormatter::success([$file], 'File successfully uploaded');
+            }
+    }
+
+
 }
