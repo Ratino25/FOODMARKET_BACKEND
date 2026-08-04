30572137dd22c8e31dda1ea3676026989d781810
Ratino <ratinoray@gmail.com>
2026-08-04 18:17:38 +0700

    api change transaction

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\FoodController;
+use App\Http\Controllers\API\TransactionController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
@@ -14,6 +15,9 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('user', [UserController::class, 'updateProfile']);
     Route::post('user/photo', [UserController::class, 'updatePhoto']);
     Route::post('logout', [UserController::class, 'logout']);
+
+    Route::get('transaction', [TransactionController::class, 'all']);
+    Route::post('transaction/{id}', [TransactionController::class, 'update']);
 });
 
 Route::post('login', [UserController::class, 'login']);
